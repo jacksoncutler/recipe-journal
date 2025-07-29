f820f6936ccf5cc1bf029d6364ba763e225d4fae
Jackson <jackson.cutler@gmail.com>
2025-07-28 17:07:03 -0700

    Create recipe list component

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,10 +1,12 @@
+import RecipeList from './RecipeList';
+
 export default function Dashboard() {
   return (
     <section id='content' className='container'>
       <div id='hero' className='hero'>
         Recipe Journal
       </div>
-      <ul id='recipe-list' className='recipe-list'></ul>
+      <RecipeList />
     </section>
   );
 }
---
+++ src/RecipeList.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+
+type Recipe = {
+  id: string;
+  name: string;
+};
+
+// const initialList: Recipe[] = [
+//   {
+//     id: '1',
+//     name: 'Cast Iron Pepperoni Pizza',
+//   },
+//   {
+//     id: '2',
+//     name: 'Grilled Gochujang Chicken',
+//   },
+//   {
+//     id: '3',
+//     name: 'Cantonese Fried Rice',
+//   },
+// ];
+
+export default function RecipeList() {
+  const [list, _] = useState<Recipe[]>([]);
+
+  useEffect(() => {
+    // get list from storage
+    // setList(initialList);
+  }, []);
+
+  return (
+    <ul id='recipe-list' className='recipe-list'>
+      {list.map((recipe) => (
+        <li key={recipe.id}>{recipe.name}</li>
+      ))}
+    </ul>
+  );
+}
