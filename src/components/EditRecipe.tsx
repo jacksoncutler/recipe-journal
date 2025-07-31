a68e5fa16cafc6714d7a459c6b5fb90aee8017bf
Jackson <jackson.cutler@gmail.com>
2025-07-30 19:51:52 -0700

    Add boilerplate for recipe edit page

--- src/components/EditRecipe.tsx
+++ src/components/EditRecipe.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useOutletContext } from 'react-router';
 
 type Context = {
@@ -7,10 +7,42 @@ type Context = {
 
 export function EditRecipe() {
   const context = useOutletContext<Context>();
+  const [isExternal, setIsExternal] = useState<boolean>(false);
 
   useEffect(() => {
     if (!context.id) return;
+    // get recipe data from storage
+    // if external recipe:
+    if (false) {
+      setIsExternal(true);
+    } else {
+      // retrieve recipe ing/instr
+    }
   }, []);
 
-  return <></>;
+  return (
+    <>
+      <div id='recipe-title-input'>
+        <input />
+      </div>
+      {
+        // toggle button for external recipe
+        isExternal ? (
+          <>
+            {
+              // input for external recipe link
+            }
+          </>
+        ) : (
+          <>
+            {
+              // ingredients form
+              // instructions form
+            }
+          </>
+        )
+        // notes form
+      }
+    </>
+  );
 }
